d56a297d996afbbac825639d4804db8e0cdfd9f8
khang.tc <khang.tc@aris-vn.com>
2022-06-21 10:58:34 +0700

    test

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
 import Head from 'next/head'
-import Image from 'next/image'
+
 import styles from '../styles/Home.module.css'
 
 export async function getServerSideProps(context) {
-
   return { props: { test: 'adasdsadsadasdsa' } }
 }
 
